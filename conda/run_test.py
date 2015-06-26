380a11f3dcbf29aa4fd54aa664732173db84280e
Patrick Snape <patricksnape@gmail.com>
2015-06-26 13:01:57 +0100

    Remove try catch from run_test.py
    It exists with code 1 when an exception is thrown anyway. This
    enables viewing of the reason for the exception!

--- conda/run_test.py
+++ conda/run_test.py
@@ -4,10 +4,8 @@ import numpy as np
 from scipy import misc
 
 if __name__ == "__main__":
-    try:
-        lena = misc.lena().astype(np.uint8)[..., None]
-        results = cyffld2.detect_frontal_faces(lena)
-        print('Found {} faces'.format(len(results)))
-        assert(len(results) == 1)
-    except:
-        sys.exit(1)
+    lena = misc.lena().astype(np.uint8)[..., None]
+    results = cyffld2.detect_frontal_faces(lena)
+    print('Found {} faces'.format(len(results)))
+    assert(len(results) == 1)
+
