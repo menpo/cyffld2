5e9132f7bc1564bef9a49284e389ffad7a56a44b
Patrick Snape <patricksnape@gmail.com>
2015-11-26 14:23:38 +0000

    Update Headhunter DPM given bug fix

--- conda/run_test.py
+++ conda/run_test.py
@@ -5,7 +5,7 @@ from scipy import misc
 
 if __name__ == "__main__":
     lena = misc.lena().astype(np.uint8)[..., None]
-    results = cyffld2.detect_frontal_faces(lena)
+    results = cyffld2.detect_frontal_faces(lena, threshold=2.)
     print('Found {} faces'.format(len(results)))
     assert(len(results) == 1)
 
